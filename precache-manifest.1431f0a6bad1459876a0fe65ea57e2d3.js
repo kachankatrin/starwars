e0db074d13ca222bc805219c6e84bbb60bef8fd5
Kateryna Nechyporchuk <kate@Katerynas-MacBook-Pro.local>
2020-07-27 02:48:24 +0300

    Updates

---
+++ precache-manifest.1431f0a6bad1459876a0fe65ea57e2d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08a2d212f7c3f23d33df3e22a4a5412",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "1bbeabc1e3280d572b01",
+    "url": "/starwars/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aab2fa9fe93177faa02f",
+    "url": "/starwars/static/css/main.72b1bdd9.chunk.css"
+  },
+  {
+    "revision": "1bbeabc1e3280d572b01",
+    "url": "/starwars/static/js/2.2d95417b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/starwars/static/js/2.2d95417b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab2fa9fe93177faa02f",
+    "url": "/starwars/static/js/main.2b01dd29.chunk.js"
+  },
+  {
+    "revision": "d89f609de804d9270792",
+    "url": "/starwars/static/js/runtime-main.b84102f1.js"
+  }
+]);
